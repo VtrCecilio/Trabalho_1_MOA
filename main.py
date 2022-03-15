0bf7866f9891cc187faf79e4a78a792a9b1c1981
Luisparf <58219058+Luisparf@users.noreply.github.com>
2022-03-15 17:18:42 -0300

    update data input

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+# from constructiveModule import calculateDistance
+
 if __name__ == '__main__':
 
     lines = []
+    dict = {}
     while (line := str(input())) != "EOF":
-        line = line.replace('\r', '')
-        lines.append(line)
+        line = line.replace('\r', '').split()
+        lines.append(line.copy())
 
     del lines[0:5]
+
+    #print(lines)
+
+    for i in range(1, len(lines)):
+        dict["node"] = lines[i][0]  # sei lá se usaremos...
+        dict["x"] = lines[i][1]  # same
+        dict["y"] = lines[i][2]  # same ^
+        lines[i] = dict.copy()
+
     print(lines)
