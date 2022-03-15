30b896cf3acb5449e38052e58b911f8c43f2ab70
Luisparf <58219058+Luisparf@users.noreply.github.com>
2022-03-15 20:32:21 -0300

    entrada de dados refatorada

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scipy.spatial import distance
+from input import fileinput ,runinput
 
 if __name__ == '__main__':
 
@@ -6,36 +7,13 @@ if __name__ == '__main__':
     # b = (4, 5)
     # print(distance.euclidean(a, b))
 
-    lines = []
+    # lines = fileinput()
+    lines = runinput()
     dict = {}
 
-    ############################################# INPUT BY FILE ###############################################
-
-    f = open('1.in', 'r')
-    lines = f.readlines()
-
-    del lines[0:5]
+    print(lines)
 
     for i in range(1, len(lines)):
-        line = lines[i].replace('\r', '').replace('.', '').split()
-        lines[i] = line
-
-    ############################################ RUN.CODES INPUT ##############################################
-
-    '''
-    while (line := str(input())) != "EOF":
-      line = line.replace('\r', '').replace('.', '').split()
-      lines.append(line.copy())
-
-    del lines2[0:5]
-
-    print(lines)
-    '''
-    ############################################################################################################
-
-    # print(lines)
-
-    for i in range(1, len(lines)-1):
         dict["node"] = lines[i][0]  # sei lá se usaremos...
         dict["x"] = lines[i][1]  # same
         dict["y"] = lines[i][2]  # same ^
@@ -57,8 +35,3 @@ if __name__ == '__main__':
         a = (x1, y1)
         b = (x2, x2)
         print("\nEuclidian distance between {} and {} is {}\n".format(a, b, distance.euclidean(a, b)))
-
-
-    
-
-    f.close()
