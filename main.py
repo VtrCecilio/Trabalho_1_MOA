56a30c13a8542b4b67961e4cc2bc7ba962b9e76d
Luisparf <58219058+Luisparf@users.noreply.github.com>
2022-03-15 22:14:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from input import fileinput ,runinput
 
 if __name__ == '__main__':
 
-    # a = (1, 2)
-    # b = (4, 5)
-    # print(distance.euclidean(a, b))
-
     # lines = fileinput()
     lines = runinput()
     dict = {}
@@ -17,21 +13,16 @@ if __name__ == '__main__':
         dict["node"] = lines[i][0]  # sei lá se usaremos...
         dict["x"] = lines[i][1]  # same
         dict["y"] = lines[i][2]  # same ^
-        print("\nDicionario: {}".format(dict))
 
         lines[i] = dict.copy()
-        print("Linha:{}".format(lines[i]))
+        print(lines[i])
 
         x1 = int(lines[1]['x'])
-        print("\nx = {}".format(x1))
         y1 = int(lines[1]['y'])
-        print("y = {}".format(y1))
 
         x2 = int(lines[i]['x'])
-        print("\nx = {}".format(x2))
         y2 = int(lines[i]['y'])
-        print("y = {}".format(y2))
 
         a = (x1, y1)
         b = (x2, x2)
-        print("\nEuclidian distance between {} and {} is {}\n".format(a, b, distance.euclidean(a, b)))
+        print("Euclidian distance between node {}{} and node {}{} is {}\n".format(1, a, i, b, distance.euclidean(a, b)))
